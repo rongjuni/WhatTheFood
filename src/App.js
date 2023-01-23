b66724db7ed14509c1d075faec43c21be68ba07e
Matthew Seo <rongjuni@hotmail.com>
2023-01-23 00:31:37 -0500

    function finish. css styling WIP

--- src/App.js
+++ src/App.js
@@ -3,37 +3,76 @@ import "./App.css";
 import Recipe from "./Recipe";
 import axios from "axios";
 
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import DirectionsIcon from "@mui/icons-material/Directions";
+
 function App() {
   let APP_ID = "7e8783af";
   let APP_KEY = "d48bee7f72049cce2b1d03458d05038b";
 
-  const [recipe, setRecipe] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
-    getRecipe();
-  }, []);
+    getRecipes();
+  }, [query]);
 
-  const getRecipe = async () => {
+  const getRecipes = async () => {
     const response = await axios.get(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
-    setRecipe(response.data.hits);
+    setRecipes(response.data.hits);
     console.log(response.data.hits);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const updateQuery = (e) => {
+    e.preventDefault();
+    setQuery(search);
   };
 
   return (
     <div className="App">
-      <h1>Let's start "What The Food"</h1>
-      <form>
-        <input type="test" />
-        <button type="button">Search</button>
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      >
+        <IconButton sx={{ p: "10px" }} aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Google Maps"
+          inputProps={{ "aria-label": "search google maps" }}
+        />
+        <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton color="primary" sx={{ p: "10px" }} aria-label="directions">
+          <DirectionsIcon />
+        </IconButton>
+      </Paper>
+      <form onSubmit={updateQuery}>
+        <input type="text" value={search} onChange={updateSearch} />
+        <button type="submit">Search</button>
       </form>
-      {recipe.map((recipe, int) => (
+      {recipes.map((recipes, int) => (
         <Recipe
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
-          ingridient={recipe.recipe.ingredientLines}
+          title={recipes.recipe.label}
+          calories={recipes.recipe.calories}
+          image={recipes.recipe.image}
+          ingridient={recipes.recipe.ingredientLines}
           int={int}
         />
       ))}
